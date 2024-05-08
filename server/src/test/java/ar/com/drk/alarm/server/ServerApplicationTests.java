248dea97e2731b8950d22ce57e8f50f5ab2e9442
Leandro Fernandez <leandro_hernan.fernandez@roche.com>
2024-05-08 19:35:50 -0300

    Add server skeleton

---
+++ server/src/test/java/ar/com/drk/alarm/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.drk.alarm.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
