3e52b86a26dfba039f6ab2f5b880fce33558d7cf
Leandro Fernandez <leandro.fernandez@roche.com>
2024-08-12 00:08:08 -0300

    Hostane to local_config

--- client/main/config.h
+++ client/main/config.h
@@ -4,13 +4,13 @@
 // The local_config.h file must define:
 // #define CONFIG_WIFI_SSID "desired-ssid"
 // #define CONFIG_WIFI_PASSWORD "password"
+// #define SERVER_HOSTNAME "hostname"
 
 #define WIFI_SCAN_METHOD WIFI_ALL_CHANNEL_SCAN
 #define WIFI_CONNECT_AP_SORT_METHOD WIFI_CONNECT_AP_BY_SIGNAL
 #define CONFIG_WIFI_SCAN_RSSI_THRESHOLD -127
 #define WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_OPEN
 
-#define SERVER_HOSTNAME "***REMOVED***"
 #define SERVER_PORT 2525
 
 #define POLL_PERIOD_MS 3000
